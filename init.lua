812c5ee79619b771b4e8597dd5339715ac4a9e28
parthokr <partho.rajvor2016@gmail.com>
2024-03-31 22:51:42 +0600

    Updated with vim-options and cp

--- init.lua
+++ init.lua
@@ -11,7 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("keymaps")
+require("vim-options")
+require("cp")
 require("lazy").setup("plugins")
 
 -- require("core.plugin")
