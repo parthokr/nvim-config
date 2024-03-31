79152429b7cbbf0c00cf2042d01921ce8f042aad
parthokr <partho.rajvor2016@gmail.com>
2024-04-01 02:07:13 +0600

    Initial commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "gopls",
+          "clangd",
+          "pyright",
+          "pylsp",
+          "bashls",
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      -- setup servers
+      lspconfig.lua_ls.setup({})
+      lspconfig.gopls.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.pylsp.setup({})
+      lspconfig.bashls.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
