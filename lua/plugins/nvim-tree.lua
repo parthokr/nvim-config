5c1cfa2ae20be93cba18d4aba9013bdc342d1398
parthokr <partho.rajvor2016@gmail.com>
2024-04-01 02:07:38 +0600

    Added nui.nvim

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,6 +2,7 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim"
     },
     config = function()
         require("nvim-tree").setup()
