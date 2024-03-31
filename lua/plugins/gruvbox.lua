dc16d5714e2546eedef20d21cc91660b86263706
parthokr <partho.rajvor2016@gmail.com>
2024-03-31 22:33:01 +0600

    Finalized

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,8 @@
+return {
+"ellisonleao/gruvbox.nvim",
+config = function()
+  vim.cmd("colorscheme gruvbox")
+    vim.g.gruvbox_contrast_dark = "hard"
+    -- vim.g.gruvbox_contrast_light = "hard"
+end,
+}
--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,5 +2,10 @@ return {
     "rcarriga/nvim-notify",
     config = function()
         vim.notify = require("notify")
+    -- auto hide after 500ms
+    vim.notify.setup({
+        stages = "fade_in_slide_out",
+        timeout = 500,
+    })
     end
 }
