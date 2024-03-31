67d02ada265a8ccf414951b4a324a84d42665722
parthokr <partho.rajvor2016@gmail.com>
2024-03-31 22:50:25 +0600

    Added vim-options

---
+++ lua/vim-options.lua
@@ -0,0 +1,24 @@
+vim.cmd("set expandtab")
+
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set autoindent")
+vim.cmd("set smartindent")
+vim.cmd("set smarttab")
+vim.cmd("set smartcase")
+vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set mouse=a")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set cursorline")
+vim.cmd("set colorcolumn=80")
+vim.cmd("set statuscolumn=%l\\ %r")
+vim.opt.termguicolors = true
+
+vim.g.mapleader = " "
+
+-- clear search highlight with <leader>h
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', {})
+
+
