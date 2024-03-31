19cf5ac477e86b44435574220dc35bf208eb8a3e
parthokr <partho.rajvor2016@gmail.com>
2024-03-31 22:18:45 +0600

    Modularized plugins

--- init.lua
+++ init.lua
@@ -1,10 +1,21 @@
-require("core.keymap")
-require("core.plugin")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system(({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath
+	}))
+end
+vim.opt.rtp:prepend(lazypath)
 
-local notify = require("notify")
+require("keymaps")
+require("lazy").setup("plugins")
 
-vim.notify = notify
+-- require("core.plugin")
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+-- local builtin = require("telescope.builtin")
+-- vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
