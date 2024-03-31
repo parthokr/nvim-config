9a7b48b996f78353348a767a5eb0a623ed18b515
parthokr <partho.rajvor2016@gmail.com>
2024-04-01 02:09:40 +0600

    Leveraging telescope-ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,5 @@
 return {
     {
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-    {
       "nvim-telescope/telescope.nvim",
       tag = "0.1.5",
       dependencies = { "nvim-lua/plenary.nvim" },
@@ -17,8 +14,20 @@ return {
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>f", builtin.find_files, {})
         vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
-  
         require("telescope").load_extension("ui-select")
       end,
     },
+    {
+      "nvim-telescope/telescope-ui-select.nvim",
+      config = function()
+        require("telescope").setup({
+          extensions = {
+            ["ui-select"] = {
+              require("telescope.themes").get_dropdown({}),
+            },
+          },
+        })
+        require("telescope").load_extension("ui-select")
+      end
+    }
   }
