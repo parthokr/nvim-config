2f8ed43b789047fe940ac44fef7c57c82b388fe3
parthokr <partho.rajvor2016@gmail.com>
2024-04-01 15:00:46 +0600

    Updated unix icon

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,28 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-        require("lualine").setup({
-            options = {
-                icons_enabled = true,
-                theme = "dracula",
-            },
-            sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "branch" },
-                lualine_c = { "filename" },
-                lualine_x = { "encoding", "fileformat", "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" },
-            },
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "dracula",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { "filename" },
+				lualine_x = {
+					"encoding",
+					{
+						"fileformat",
+						symbols = { unix = "", dos = "" },
+					},
+					"filetype",
+					"diff",
+					"diagnostics",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
