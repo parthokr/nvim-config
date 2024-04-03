00900941e5ef2398e29f642d503970be8a297015
parthokr <partho.rajvor2016@gmail.com>
2024-04-03 16:36:42 +0600

    Finalized

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,13 +12,16 @@ vim.cmd("set mouse=a")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set cursorline")
-vim.cmd("set colorcolumn=80")
-vim.cmd("set statuscolumn=%l\\ %r")
+-- vim.cmd("set colorcolumn=80")
+-- vim.cmd("set statuscolumn=%l\\ %r")
 vim.opt.termguicolors = true
 
 vim.g.mapleader = " "
 
 -- clear search highlight with <leader>h
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', {})
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", {})
 
-
+-- disable github copilot on leader + dc
+vim.keymap.set("n", "<leader>dc", ":Copilot disable<CR>", {})
+-- enable github copilot on leader + ec
+vim.keymap.set("n", "<leader>ec", ":Copilot enable<CR>", {})
