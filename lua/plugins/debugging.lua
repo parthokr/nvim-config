2db6f279eebe50a5c8ab70b1c617a3664356dfa6
parthokr <partho.rajvor2016@gmail.com>
2024-04-04 16:23:11 +0600

    Added golang dap

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,32 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"leoluz/nvim-dap-go",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+		require("dapui").setup()
+		require("dap-go").setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<leader>B", dap.set_breakpoint, {})
+		vim.keymap.set("n", "<F5>", dap.continue, {})
+		vim.keymap.set("n", "<F10>", dap.step_over, {})
+		vim.keymap.set("n", "<F11>", dap.step_into, {})
+		vim.keymap.set("n", "<S-F11>", dap.step_out, {})
+	end,
+}
