7c652ba366c3604592e34b60aa3e0d1f1a5abf51
parthokr <partho.rajvor2016@gmail.com>
2024-04-04 16:23:38 +0600

    Transparent mode enabled

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,8 +1,13 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  config = function()
-    -- vim.cmd("colorscheme gruvbox")
-    -- vim.g.gruvbox_contrast_dark = "hard"
-    -- vim.g.gruvbox_contrast_light = "hard"
-  end,
+	"ellisonleao/gruvbox.nvim",
+	config = function()
+		require("gruvbox").setup({
+			style = "dark",
+			contrast = "hard",
+			transparent_mode = true,
+		})
+		vim.cmd("colorscheme gruvbox")
+		-- vim.g.gruvbox_contrast_dark = "hard"
+		-- vim.g.gruvbox_contrast_light = "hard"
+	end,
 }
