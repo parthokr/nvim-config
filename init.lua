7629a8dc53fc35138ac0b752f31fe090ec0a6c1b
parthokr <partho.rajvor2016@gmail.com>
2024-04-07 22:34:06 +0600

    Fixed keymap

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system(({
+	vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable",
-		lazypath
-	}))
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -20,3 +20,9 @@ require("lazy").setup("plugins")
 -- local builtin = require("telescope.builtin")
 -- vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 -- vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+
+-- make background transparent
+-- vim.cmd("highlight Normal guibg=NONE")
+-- vim.cmd("highlight NonText guibg=NONE")
+-- vim.cmd("highlight SignColumn ctermbg=NONE")
+-- vim.cmd("highlight NonText ctermbg=NONE")
